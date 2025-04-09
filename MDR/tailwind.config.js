de7980405800e0ac1cc15d059bbeca8afb7a7b4f
William Bernal <wbernal2@huskers.unl.edu>
2025-04-09 15:00:45 -0500

    boring

---
+++ MDR/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"Share Tech Mono"', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
